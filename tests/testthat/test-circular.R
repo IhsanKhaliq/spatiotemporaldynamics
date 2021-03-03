e1502581af0081ca6f8a8c8ed22cc022241bf735
IhsanKhaliq <ihsan.khaliq@usq.edu.au>
2021-03-04 08:51:27 +1000

    ""

--- tests/testthat/test-circular.R
+++ tests/testthat/test-circular.R
@@ -1,3 +1,8 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+context("circular.averaging()")
+
+# Test that circular.averaging() returns expected results
+test_that("circular.averaging returns expected results", {
+  identical(circular.averaging(c(0, 90, 180, 270)), NA)
+  identical(circular.averaging(c(70, 82, 96, 110, 119, 259)), 99.68)
 })
